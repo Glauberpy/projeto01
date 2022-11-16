2213558978d67016d1da133e8c4815042f03a79b
Glauberpy <118091286+Glauberpy@users.noreply.github.com>
2022-11-15 23:07:59 -0300

    verificar qual combustível compensa mais

---
+++ combustivel.py
@@ -0,0 +1,20 @@
+gasolina = float(input('Digite o preço da gasolina \n'))
+
+alcool = float(input('Digite o preço do alcool \n'))4
+
+resultado= alcool/ gasolina *(70/100)*100
+
+print(resultado)
+
+if resultado <70:
+    print('Compensa a gasolina')
+    
+elif  resultado >70:
+    print('Compensa o alcool')
+    
+elif  resultado == 70:
+    print('Tanto faz')
+
+consultar = input ('Deseja fazer uma nova consulta ?')
+
+
